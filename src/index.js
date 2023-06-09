0e65f61b8bb890452530b2c35fa1832fbaf31a2d
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-06-09 15:44:04 +0400

    feat: impliment solvetion for algoritmic task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+    if (matrix === []) return []
+    return matrix.reduce(((acc, el, index) => {
+        if ( index % 2 === 0) {
+            return acc = [...acc, ...el]
+        } else {
+            for (let i = el.length -1; i >= 0; i--) {
+                acc = [...acc, el[i]]
+            }
+            return acc
+        }
+    }),[])
 }
